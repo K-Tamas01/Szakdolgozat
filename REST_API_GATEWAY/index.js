09466ca379aa35e6aa64ab5337e10a7c69eff168
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-04-15 20:10:13 +0200

    remove gateway and add nginx

--- REST_API_GATEWAY/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const httpProxy = require('http-proxy');
-
-const app = express();
-
-app.listen(5000, () => {
-    console.log('REST API gateway started on port 5000');
-});
