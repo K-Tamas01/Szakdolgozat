e1afd74a7022b7e846819bd4fa9fe4333c3042e3
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-07-26 18:43:16 +0200

    Role és User hozzáadása

--- DOCKER/mongo/Init/init.js
+++ DOCKER/mongo/Init/init.js
@@ -3,3 +3,25 @@ db.createCollection("users", {capped: false});
 db.createCollection("products", {capped: false});
 db.createCollection("delivery", {capped: false});
 db.createCollection("billing", {capped: false});
+
+db.createRole({
+    role: "admin",
+    privileges: [
+        { resource: {
+            db: "Storage-Manager-System",
+            collection: ""
+            },
+            actions: [ "find", "update", "insert", "remove"]
+        }
+    ],
+    roles: []
+})
+
+db.createUser({
+    user: "rootAdmin",
+    pwd: "rootAdmin123",
+    roles:[{
+        role: "admin",
+        db: "Storage-Manager-System"
+    }]
+})
