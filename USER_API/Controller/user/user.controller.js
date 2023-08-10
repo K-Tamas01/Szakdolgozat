80eeec9316eb918084416eafd1466fa64b5360c7
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-08-10 17:28:24 +0200

    Update user.controller.js

--- USER_API/Controller/user/user.controller.js
+++ USER_API/Controller/user/user.controller.js
@@ -4,65 +4,67 @@ const md5 = require('md5')
 const userModel = require('../../model/user.schema')
 
 const userLogin = async (req, res) => {
-    const { username, password } = req.body
-    const result = await userModel.findOne({Uname: username})
+    const { email, password } = req.body
+    const result = await userModel.findOne({email: email})
 
-    if(!result) res.status(400).send("Invalid username!")
-    if(result.password == md5(password)) res.status(400).send("Invalid password!")
+    if(!result) {res.status(500).send("Invalid email!"); return}
+    if(result.password !== md5(password)) {res.status(500).send("Invalid password!"); return}
 
     res.status(200).send("Login Success")
 }
 
 const userRegist = async (req, res) => {
-    const { username, password, email } = req.body
+    const { firstName, lastName, password, email, address, city, mobile, zipCode } = req.body
     
     const emailResult = await userModel.findOne({email: email})
 
     if(!!emailResult) { 
-        res.status(400).send("Email is already taken"); 
+        res.status(500).send("Email is already taken"); 
         return; 
-    }
-    
-    const UnameResult = await userModel.findOne({Uname: username})
-
-    if(!!UnameResult) {
-        res.status(400).send("Username is already taken");
-        return;
     }
 
     const result = new userModel({
         email: email,
-        Uname: username,
-        password: md5(password)
+        firstName: firstName,
+        lastName: lastName,
+        password: md5(password),
+        address: address,
+        city: city,
+        mobile: mobile,
+        zipCode: zipCode
     })
 
     try{
         await result.save()
     } catch(e){
-        res.status(400).send({msg: e});
-        return;
+        res.status(500).send({msg: e});
+        return
     }
 
     res.status(201).send("Registration success")
 }
 
 const userUpdate = async (req, res) => {
+    const newData = req.body
 
+    const result = await userModel.findOneAndUpdate({_id: newData._id}, {$set: newData}, {upsert: false})
+
+    result ? res.status(500).send('Failed....') : res.status(200).send('Success')
 }
 
 const userDelete = async (req, res) => {
+    const { email } = req.body
 
-}
+    const userResult = await userModel.findOneAndDelete({email: email})
 
-const getUser = async (req, res) => {
+    if(userResult.deletedCount === 0) return res.code(500).send('Failed...');
 
+    res.code(200).send('Success');
 }
 
 userController.post("/login", userLogin)
 userController.post("/regist", userRegist)
 userController.put("/update", userUpdate)
 userController.delete("delete", userDelete)
-userController.get("getUser", getUser)
-
 
 module.exports = userController
