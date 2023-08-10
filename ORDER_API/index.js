24b41c1e629ca9c0ab4420b2f2f098ff0b381e17
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-08-10 17:28:13 +0200

    add controller

--- ORDER_API/index.js
+++ ORDER_API/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./plugin/database/db');
+const orderController = require('./controller/order/order.controller')
 require('dotenv').config();
 
 const app = express();
@@ -9,9 +10,7 @@ app.use(cors())
 
 connectDB()
 
-app.get("/api/order/teszt", (req, res) => {
-    res.send("Order is working")
-})
+app.use('/api/order', orderController)
 
 app.listen(process.env.PORT, () => {
     console.log('Order API started on port ' + process.env.PORT);
