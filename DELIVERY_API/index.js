9c1c7560ac46b8335257a8c8a4fbf46701711e7b
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2023-08-23 15:32:38 +0200

    stockmovements delete

--- DELIVERY_API/index.js
+++ DELIVERY_API/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./plugin/database/db');
 const deliveryController = require('./controller/delivery/delivery.controller')
-const movementsController = require('./controller/movements/movements.controller')
 require('dotenv').config();
 
 const app = express();
@@ -11,8 +10,7 @@ app.use(cors())
 
 connectDB()
 
-// app.use('/api/delivery', deliveryController)
-// app.use('/api/movements', movementsController)
+app.use("/api/delivery", deliveryController)
 
 app.listen(process.env.PORT, () => {
     console.log('Delivery and Billing API started on port ' + process.env.PORT);
